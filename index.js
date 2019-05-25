f7af43e8f057f33fc1a701e79b7017633a58699d
Davi Paranaguá <daviparanagua@gmail.com>
2019-05-25 11:58:32 -0300

    Algoritmo de extração OK

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const pdf = require('pdf-parse');
 
 const fileURL = 'http://www.ans.gov.br/images/stories/Plano_de_saude_e_Operadoras/tiss/Padrao_tiss/tiss3/padrao_tiss_componente_organizacional_201902.pdf';
 
+const FIRST = 0, WHITE = 1, SKIPPED = 2, OK = 3;
+
 download(fileURL, './tmp_tiss.pdf', processFile);
 
 
@@ -15,17 +17,47 @@ function processFile(file) {
         let exportedText = data.text;
         let splittedText = exportedText.split(/(?:\r\n|\r|\n)/g);
         let started = false;
+        let output = [];
 
-        for(row of splittedText){
-            if (row.match('Quadro 31')){ started = true; console.log(row); }
+        let lastStatus = FIRST;
 
-            if(started) {                
-                if(row.match(/([0-9]+) (.+)/)) {console.log(row);}
-                if (row.match('Fonte: Elaborado pelos autores.')){ break; }
+        let startString = 'Quadro 31';
+        let endString = 'Fonte: Elaborado pelos autores.';
+
+        for(rowIndex in splittedText){
+            let row = splittedText[rowIndex];
+
+            // Localizar fator de início de transcrição
+            if (row.match(startString)){ started = true; console.log(row); }
+
+            // Apenas após iniciar a transcrição
+            else if(started) {
+
+                // Linha de encerramento
+                if (row.match(endString)){ console.log(endString); break;  }
+
+                // Linha em branco - sinalizador de quebra de página iminente
+                if (row.match(/^\s+$/)) { lastStatus = WHITE; console.log(row);}
+
+                // Linha anterior em branco. Irrelevante.
+                else if (lastStatus == WHITE) {lastStatus = SKIPPED; continue;}
+
+                // Linha completa. OK
+                else if(row.match(/^([0-9]+) (.*)/)) {
+                    output.push(row);
+                    lastStatus = OK;
+                }
+
+                // Linha residual do registro anterior
+                else if(row.match(/(.+)/) && lastStatus === OK) {
+                    output[output.length - 1] = output[output.length - 1] + ' ' + row;
+                }
             }
         }
 
         fs.writeFile('./file.txt', exportedText, () => {});
+        fs.writeFile('./finalFile.txt', output.join("\r\n"), () => {});
+        console.log(`${output.length} registros encontrados`);        
     });
     
 }
