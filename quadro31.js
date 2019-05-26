83ecc9e1c2aa459fbaf013fd5c3d5aa5cb02472c
Davi Paranaguá <daviparanagua@gmail.com>
2019-05-25 21:10:32 -0300

    Reorganizado em promises

--- quadro31.js
+++ quadro31.js
@@ -14,100 +14,124 @@ module.exports = function(moduleCB) {
 
     const FIRST = 0, WHITE = 1, SKIPPED = 2, OK = 3;
 
-    download(fileURL, tmpPdfFilename, processFile);
+    // CONEXÃO
 
-    function processFile(file) {
-        console.log('Processando arquivo...');
-        let dataBuffer = fs.readFileSync(file);
-
-        pdf(dataBuffer).then(function(data) {
-            let exportedText = data.text;
-            let splittedText = exportedText.split(/(?:\r\n|\r|\n)/g);
-            let started = false;
-            let output = [];
-
-            let lastStatus = FIRST;
-
-            let startString = /Quadro 31/;
-            let endString = /Fonte: Elaborado pelos autores./;
-
-            for(rowIndex in splittedText){
-                let row = splittedText[rowIndex];
-
-                // Localizar fator de início de transcrição
-                if (row.match(startString)){ started = true;}
-
-                // Apenas após iniciar a transcrição
-                else if(started) {
-
-                    // Linha de encerramento
-                    if (endString.test(row)){ break; }
-
-                    // Linha em branco - sinalizador de quebra de página iminente
-                    if (/^\s+$/.test(row)) { lastStatus = WHITE;}
-
-                    // Linha anterior em branco. Irrelevante.
-                    else if (lastStatus == WHITE) {lastStatus = SKIPPED; continue;}
-
-                    // Linha completa. OK
-                    else if(/^([0-9]+) (.*)/.test(row) ) {
-                        matched = row.match(/^([0-9]+) (.*)/);
-                        output.push(`${matched[1]};${matched[2].trim()}`);
-                        lastStatus = OK;
-                    }
-
-                    // Linha residual do registro anterior
-                    else if(/(.+)/.test(row) && lastStatus === OK) {
-                        output[output.length - 1] = output[output.length - 1] + ' ' + row.trim();
-                    }
-                }
-            }
-            
-            fs.writeFile(tmpCsvFilename, output.map((value) => value.replace('; ', ';')).join("\n"), saveInDatabase);
-            console.log(`${output.length} registros encontrados`);
-
-            fs.unlink(tmpPdfFilename, ()=>{});
-        });
-        
-    }
-
-    function download(url, dest, cb) {
-    var file = fs.createWriteStream(dest);
-    console.log('Obtendo arquivo...');
-    var request = http.get(url, function(response) {
-        response.pipe(file);
-        file.on('finish', function() {
-            file.close();
-            cb(dest);
-        });
-    }).on('error', function(err) { // Handle errors
-        fs.unlink(dest);
-        if (cb) cb(err.message);
+    var mysql = require('mysql');
+    const connection = mysql.createConnection({
+        host     : mySQL_host,
+        user     : mySQL_user,
+        password : mySQL_pass,
+        database : mySQL_database,
+        charset: "utf8_general_ci"
     });
+
+    connection.connect();
+
+    download(fileURL, tmpPdfFilename)
+        .then(parseFile)
+        .then((file) => saveInDatabase(file, 'quadro31') );
+
+    /**
+     * Download a file and save it to dest
+     * @param {*} url File to be downloaded
+     * @param {*} dest Local path to save file
+     */
+    function download(url, dest) {
+        return new Promise ((resolve, reject) => {
+            var file = fs.createWriteStream(dest);
+            console.log('Obtendo arquivo...');
+            http.get(url, function(response) {
+                response.pipe(file);
+                file.on('finish', function() {
+                    file.close( () => resolve(dest) );
+                });
+            }).on('error', function(err) { // Handle errors
+                fs.unlink(dest);                
+                reject(false);
+            });
+        });
     };
 
-    function saveInDatabase(dataArray){
-        var mysql = require('mysql');
-        const connection = mysql.createConnection({
-            host     : mySQL_host,
-            user     : mySQL_user,
-            password : mySQL_pass,
-            database : mySQL_database,
-            charset: "utf8_general_ci"
-        });
+    /**
+     * Parses a file, extracting relevant data from PDF downloaded
+     * 
+     * @param {*} file 
+     */
+    function parseFile(file) {
+        return new Promise((resolve, reject) => {
+            console.log('Processando arquivo...');
+            let dataBuffer = fs.readFileSync(file);
 
-        connection.connect();
+            pdf(dataBuffer).then(function(data) {
+                let exportedText = data.text;
+                let splittedText = exportedText.split(/(?:\r\n|\r|\n)/g);
+                let started = false;
+                let output = [];
 
-        connection.query(`TRUNCATE TABLE quadro31`, function (error, results, fields) {
-            if (error) throw error;
+                let lastStatus = FIRST;
 
-            connection.query(`LOAD DATA LOCAL INFILE ? INTO TABLE quadro31 FIELDS TERMINATED BY ';'`, [tmpCsvFilename], function (error, results, fields) {
-                fs.unlink(tmpCsvFilename, ()=>{});
-                if (error) throw error;
-                if(moduleCB) { moduleCB(); }
+                let startString = /Quadro 31/;
+                let endString = /Fonte: Elaborado pelos autores./;
+
+                for(rowIndex in splittedText){
+                    let row = splittedText[rowIndex];
+
+                    // Localizar fator de início de transcrição
+                    if (row.match(startString)){ started = true;}
+
+                    // Apenas após iniciar a transcrição
+                    else if(started) {
+
+                        // Linha de encerramento
+                        if (endString.test(row)){ break; }
+
+                        // Linha em branco - sinalizador de quebra de página iminente
+                        if (/^\s+$/.test(row)) { lastStatus = WHITE;}
+
+                        // Linha anterior em branco. Irrelevante.
+                        else if (lastStatus == WHITE) {lastStatus = SKIPPED; continue;}
+
+                        // Linha completa. OK
+                        else if(/^([0-9]+) (.*)/.test(row) ) {
+                            matched = row.match(/^([0-9]+) (.*)/);
+                            output.push(`${matched[1]};${matched[2].trim()}`);
+                            lastStatus = OK;
+                        }
+
+                        // Linha residual do registro anterior
+                        else if(/(.+)/.test(row) && lastStatus === OK) {
+                            output[output.length - 1] = output[output.length - 1] + ' ' + row.trim();
+                        }
+                    }
+                }
+                
+                console.log(`${output.length} registros encontrados`);
+                fs.writeFile(tmpCsvFilename, output.map((value) => value.replace('; ', ';')).join("\n"), () => resolve(tmpCsvFilename) );
+                fs.unlink(tmpPdfFilename, ()=>{});
             });
+        });
+    }
 
-            connection.end();        
+    /**
+     * Loads the csv to the remote table
+     * @param {*} tmpCsvFilename 
+     */
+    function saveInDatabase(filename, table){
+        console.log(`Carregando ${filename} em ${table}`);
+
+        return new Promise((resolve, reject) => {
+            connection.query(`TRUNCATE TABLE ${table}`, function (error, results, fields) {
+                if (error) throw error;
+
+                connection.query(`LOAD DATA LOCAL INFILE ? INTO TABLE ${table} FIELDS TERMINATED BY ';'`, [filename, table], function (error, results, fields) {
+                    fs.unlink(tmpCsvFilename, ()=>{});
+                    if (error) throw error;
+                    if(moduleCB) { moduleCB(); }
+                });
+
+                connection.end();
+                resolve();
+            });
         });
     }
 }
